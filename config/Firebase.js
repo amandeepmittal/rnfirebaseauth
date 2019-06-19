a8ec31ddf0e70e4207f5c5d1ab5145e437dd61b4
Aman Mittal <amandeepmittal@live.com>
2019-06-19 11:40:14 +0530

    add env variables

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,16 +1,24 @@
 import firebase from 'firebase'
+import {
+	API_KEY,
+	AUTH_DOMAIN,
+	DATABASE_URL,
+	PROJECT_ID,
+	MESSAGE_SENDER_ID,
+	APP_ID
+} from 'react-native-dotenv'
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyClsBKWOOXfzeb48Rm80hX09X87J_TB4mM',
-	authDomain: 'rnexpo0.firebaseapp.com',
-	databaseURL: 'https://rnexpo0.firebaseio.com',
-	projectId: 'rnexpo0',
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	databaseURL: DATABASE_URL,
+	projectId: PROJECT_ID,
 	storageBucket: '',
-	messagingSenderId: '256218572662',
-	appId: '1:256218572662:web:a31e231f7265020d'
+	messagingSenderId: MESSAGE_SENDER_ID,
+	appId: APP_ID
 }
 
 // Initialize Firebase
-const Firebase = firebase.initializeApp(firebaseConfig)
+let Firebase = firebase.initializeApp(firebaseConfig)
 
 export default Firebase
